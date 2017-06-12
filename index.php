a598a071ca18956a1c24883b13bfe4db2baf04b8
Prawit Thammisorn <prawit.thammisorn@gmail.com>
2017-06-12 18:14:15 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php $result = exec("python videofacedectection.py"); // ให้ปรับเปลี่ยนไปตามค่าที่ตั้งไว้ในระบบปฏิบัติการครับ 
+print_r($result); 
+echo "Prawit Test - videofacedectection Code : ".$result
+?>
